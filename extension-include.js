434c46f8f34177d4472ad5c98d666760c2137af9
Devin Rhode <devinrhode2@gmail.com>
2012-08-10 03:47:35 -0700

    Update extension-include.js

--- extension-include.js
+++ extension-include.js
@@ -11,16 +11,16 @@
 
 //'globals':
 var getClass, getId, getTag, GET, POST, fail, log, warn, error, trackEvent, storageDefault, createElement, runInPage, nodeReady;
-(function extensionInclude(){
+(function extensionInclude() {
 'use strict';
 
-getClass = function getClass(elements){
+getClass = function getClass(elements) {
   return document.getElementsByClassName(elements);
 };
-getId = function getId(elements){
+getId = function getId(elements) {
   return document.getElementById(elements);
 };
-getTag = function getTag(elements){
+getTag = function getTag(elements) {
   return document.getElementsByTagName(elements);
 };
 HTMLElement.prototype.getClass = HTMLElement.prototype.getElementsByClassName;
@@ -39,29 +39,29 @@ ajax.send = function(u, f, m, a) {
   var x = ajax.x();
   x.open(m, u, true);
   x.onreadystatechange = function() {
-    if(x.readyState == 4) {
+    if(x.readyState === 4) {
       f(x.responseText, x);
     }
   };
-  if(m === "POST") {
-    x.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  if(m === 'POST') {
+    x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   }
-  x.send(a)
+  x.send(a);
 };
 POST = function POST(url, callback, args) {
-  ajax.send(url, func, "POST", args);
+  ajax.send(url, func, 'POST', args);
 };
 
-GET = function GET(url, callback){
-  ajax.send(url,callback,'GET');
+GET = function GET(url, callback) {
+  ajax.send(url, callback, 'GET');
 };
 
-var masterHistory = function masterHistory(){
+var masterHistory = function masterHistory() {
   //have a master switch on caller.name === 'f' (log) || 'warn' || 'fail' || 'error'
 };
 
 //fail, program cannot continue. Alert a message, and kill it
-fail = function fail(message){
+fail = function fail(message) {
   masterHistory(arguments);
   alert(message);
   throw message;
@@ -94,20 +94,20 @@ log = function f() {
 
 
 //Some common pitfall that is handled. Application will continue fine.
-warn = function warn(message){
+warn = function warn(message) {
   masterHistory(arguments);
   debug && console.warn(message);
   return message;
 };
 //some error where the program will continue, but this scenario really shouldn't be occuring
-error = function error(message){
+error = function error(message) {
   masterHistory(arguments);
   debug && console.error(message);
   return message;
 };
 
 //KISSmetrics
-trackEvent = function trackEvent(){
+trackEvent = function trackEvent() {
   if (typeof _kmq === 'undefined') {
     window._kmq = [];
   }
@@ -119,7 +119,7 @@ trackEvent = function trackEvent(){
   }
 };
 
-storageDefault = function storageDefault(arg1, arg2){
+storageDefault = function storageDefault(arg1, arg2) {
   if (typeof arg1 === 'string') {
     if (localStorage.getItem(arg1) === null) {
       localStorage.setItem(arg1, arg2);
@@ -200,7 +200,7 @@ runInPage = function runInPage() {
  * Send questions/problems/critiques on code to: DevinRhode2@gmail.com (put "skywalker.js" in the title)
  */
 
-nodeReady = function nodeReady(call, readyCallback, timeout){
+nodeReady = function nodeReady(call, readyCallback, timeout) {
   'use strict';//steppin it up!
   var box = typeof call; //our one and only var...?!
   if (box === 'string') {
